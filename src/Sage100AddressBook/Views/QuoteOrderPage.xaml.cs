9f5774826a912a470d33d66ed1158610d3c9417d
Russell Libby <russell.libby@sage.com>
2016-07-14 09:13:49 -0700

    Additional line columns displayed depending on VisualState triggers.

--- src/Sage100AddressBook/Views/QuoteOrderPage.xaml.cs
+++ src/Sage100AddressBook/Views/QuoteOrderPage.xaml.cs
@@ -4,6 +4,7 @@
 
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
 
 namespace Sage100AddressBook.Views
 {
@@ -12,6 +13,36 @@ namespace Sage100AddressBook.Views
     /// </summary>
     public sealed partial class QuoteOrderPage : Page
     {
+        #region Private methods
+
+        /// <summary>
+        /// Page loaded event.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var show = (ActualWidth >= 600);
+
+            Items.Columns[2].IsVisible = show;
+            Items.Columns[3].IsVisible = show;
+        }
+
+        /// <summary>
+        /// Visual state group changed event.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void CurrentVisualStateChanged(object sender, VisualStateChangedEventArgs e)
+        {
+            var show = (!e.NewState.Name.Equals("VisualStateNarrow"));
+
+            Items.Columns[2].IsVisible = show;
+            Items.Columns[3].IsVisible = show;
+        }
+
+        #endregion
+
         #region Constructor
 
         /// <summary>
